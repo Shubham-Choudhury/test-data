f8dff6451c3227743391727c6de4c346eb454d3d
Shubham Choudhury <shubhamchoudhury2002@yahoo.com>
2023-07-31 17:10:28 +0530

    Add

---
+++ app.js
@@ -0,0 +1,29 @@
+const info = {
+    name: "Shubham",
+    age: 21
+}
+
+const question = [
+    {
+        semester: 1,
+        id: "semester-1",
+        subjects:[
+            {
+                name: "it",
+                "subject-id": "it",
+                question:[
+                    {
+                        week: 1,
+                        paper: [
+                            {
+                                problem:"Hello World"
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+];
+
+export {info, question};
---
+++ test.js
@@ -0,0 +1,1 @@
+import {info, question} from './app.js';
